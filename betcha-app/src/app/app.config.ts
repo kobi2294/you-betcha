555a08a4e9112e760891f37720770d07a1203267
Kobi Hari <kobi2294@yahoo.com>
2024-05-22 23:51:57 +0300

    getting ready to add functions

--- betcha-app/src/app/app.config.ts
+++ betcha-app/src/app/app.config.ts
@@ -4,13 +4,7 @@ import packageJson from '../../package.json';
 
 import { routes } from './app.routes';
 import { APP_VERSION } from './tokens/app-version.token';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getFunctions, provideFunctions } from '@angular/fire/functions';
-import { getStorage, provideStorage } from '@angular/fire/storage';
-import { environment } from '../environments/environment';
-import { importAuth, importFirebase, importFirestore, importFunctions, importStorage, provideFirebaseServices } from './config/firebase';
+import { provideFirebaseServices } from './config/firebase';
 
 export const appConfig: ApplicationConfig = {
   providers: [
