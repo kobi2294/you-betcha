8876712f59b2fed6dced7e08cf9567d28d55a91c
Kobi Hari <kobi2294@yahoo.com>
2024-05-22 16:51:52 +0300

    configured angular firebae

--- betcha-app/src/environments/environment.stage.ts
+++ betcha-app/src/environments/environment.stage.ts
@@ -1,5 +1,12 @@
 export const environment = {
-    name: 'stage',
-    production: false    
-
+  name: 'stage',
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC-sVnTn7ZBTSJ7_jTEV9K9yCtPhETPnac',
+    authDomain: 'you-betcha-stage.firebaseapp.com',
+    projectId: 'you-betcha-stage',
+    storageBucket: 'you-betcha-stage.appspot.com',
+    messagingSenderId: '436644278366',
+    appId: '1:436644278366:web:8955c80cdea8fc93f404cf',
+  },
 };
--- betcha-app/src/environments/environment.ts
+++ betcha-app/src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
-    name: 'prod',
-    production: true    
+  name: 'prod',
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAUc2vtanIKSmIz2bHCUOFd3A0elIPLjN0',
+    authDomain: 'you-betcha-prod.firebaseapp.com',
+    projectId: 'you-betcha-prod',
+    storageBucket: 'you-betcha-prod.appspot.com',
+    messagingSenderId: '146921415276',
+    appId: '1:146921415276:web:f60d85dfebae386785ae49',
+  },
 };
