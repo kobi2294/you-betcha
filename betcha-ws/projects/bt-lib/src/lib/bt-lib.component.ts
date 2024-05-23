6d0b1a4b4f228bd060611f2126364a1bdce6039f
Kobi Hari <kobi2294@yahoo.com>
2024-05-23 16:21:49 +0300

    created apps and lib

---
+++ betcha-ws/projects/bt-lib/src/lib/bt-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-bt-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      bt-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class BtLibComponent {
+
+}
---
+++ betcha-ws/projects/bt-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of bt-lib
+ */
+
+export * from './lib/bt-lib.service';
+export * from './lib/bt-lib.component';
