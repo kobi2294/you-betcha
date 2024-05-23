aeb734618af20a6b979ddc44a066f3e914a2ee81
Kobi Hari <kobi2294@yahoo.com>
2024-05-23 19:26:35 +0300

    configured production sites

--- betcha-ws/projects/betcha-admin/src/environments/environment.ts
+++ betcha-ws/projects/betcha-admin/src/environments/environment.ts
@@ -3,11 +3,11 @@ export const environment = {
   production: true,
   simulation: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAUc2vtanIKSmIz2bHCUOFd3A0elIPLjN0",
-    authDomain: "you-betcha-prod.firebaseapp.com",
-    projectId: "you-betcha-prod",
-    storageBucket: "you-betcha-prod.appspot.com",
-    messagingSenderId: "146921415276",
-    appId: "1:146921415276:web:f92aec7e17ec360585ae49"
-    },
+    apiKey: 'AIzaSyAUc2vtanIKSmIz2bHCUOFd3A0elIPLjN0',
+    authDomain: 'you-betcha-prod.firebaseapp.com',
+    projectId: 'you-betcha-prod',
+    storageBucket: 'you-betcha-prod.appspot.com',
+    messagingSenderId: '146921415276',
+    appId: '1:146921415276:web:5f4c93e00f5a540b85ae49',
+  },
 };
--- betcha-ws/projects/betcha-app/src/environments/environment.ts
+++ betcha-ws/projects/betcha-app/src/environments/environment.ts
@@ -3,11 +3,11 @@ export const environment = {
   production: true,
   simulation: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAUc2vtanIKSmIz2bHCUOFd3A0elIPLjN0",
-    authDomain: "you-betcha-prod.firebaseapp.com",
-    projectId: "you-betcha-prod",
-    storageBucket: "you-betcha-prod.appspot.com",
-    messagingSenderId: "146921415276",
-    appId: "1:146921415276:web:977ca71a2b962b4385ae49"
-      },
+    apiKey: 'AIzaSyAUc2vtanIKSmIz2bHCUOFd3A0elIPLjN0',
+    authDomain: 'you-betcha-prod.firebaseapp.com',
+    projectId: 'you-betcha-prod',
+    storageBucket: 'you-betcha-prod.appspot.com',
+    messagingSenderId: '146921415276',
+    appId: '1:146921415276:web:7102b8fd02b043f785ae49',
+  },
 };
