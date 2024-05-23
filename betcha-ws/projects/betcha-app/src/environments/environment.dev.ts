37fafedfa1ca780d5b4a51cee29796121c8b93b7
Kobi Hari <kobi2294@yahoo.com>
2024-05-23 18:02:21 +0300

    modified build and added environments for 3 apps

---
+++ betcha-ws/projects/betcha-app/src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  name: 'dev',
+  production: false,
+  simulation: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC-sVnTn7ZBTSJ7_jTEV9K9yCtPhETPnac",
+    authDomain: "you-betcha-stage.firebaseapp.com",
+    projectId: "you-betcha-stage",
+    storageBucket: "you-betcha-stage.appspot.com",
+    messagingSenderId: "436644278366",
+    appId: "1:436644278366:web:facd7409503f3248f404cf"
+    },
+};
---
+++ betcha-ws/projects/betcha-app/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  name: 'prod',
+  production: true,
+  simulation: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAUc2vtanIKSmIz2bHCUOFd3A0elIPLjN0",
+    authDomain: "you-betcha-prod.firebaseapp.com",
+    projectId: "you-betcha-prod",
+    storageBucket: "you-betcha-prod.appspot.com",
+    messagingSenderId: "146921415276",
+    appId: "1:146921415276:web:977ca71a2b962b4385ae49"
+      },
+};
