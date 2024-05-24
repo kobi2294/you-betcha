94621d44e63d087c5e279c542e24277534952078
Kobi Hari <kobi2294@yahoo.com>
2024-05-24 14:31:25 +0300

    minimal ui for all apps

--- betcha-ws/projects/betcha-admin/src/app/app.config.ts
+++ betcha-ws/projects/betcha-admin/src/app/app.config.ts
@@ -2,12 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideFirebaseServices } from '@lib';
+import { provideFirebaseServices, provideVersion } from '@lib';
 import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
+    provideVersion(),
     provideFirebaseServices(environment.firebaseConfig, environment.simulation),   
   ]
 };
--- betcha-ws/projects/betcha-app/src/app/app.component.ts
+++ betcha-ws/projects/betcha-app/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { APP_VERSION } from '../../../bt-lib/src/utils/provide-version';
+import { environment } from '../environments/environment';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'betcha-app';
+  version = inject(APP_VERSION);
+  env = environment.name;
+
+  firestore = inject(Firestore);
+  msg$ = getDoc(doc(this.firestore, 'constants/data')).then(res => res.data());
 }
