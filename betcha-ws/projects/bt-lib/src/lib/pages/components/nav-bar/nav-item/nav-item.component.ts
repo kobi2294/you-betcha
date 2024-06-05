2f5d8736b8f21e3b36107745f53561c43fe21361
Kobi Hari <kobi2294@yahoo.com>
2024-06-05 15:27:11 +0300

    navbar design

--- betcha-ws/projects/bt-lib/src/lib/pages/components/nav-bar/nav-item/nav-item.component.ts
+++ betcha-ws/projects/bt-lib/src/lib/pages/components/nav-bar/nav-item/nav-item.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'lib-nav-item',
   templateUrl: './nav-item.component.html',
-  styleUrl: './nav-item.component.scss'
+  styleUrl: './nav-item.component.scss', 
+  hostDirectives: [
+    RouterLinkActive, 
+    {
+      directive: RouterLink, 
+      inputs: [
+        'routerLink: page'
+      ]
+    }
+  ]
 })
 export class NavItemComponent {
+  readonly page = input.required<string>();
+
+  readonly icon = input.required<string>();
+
+  readonly caption = input.required<string>();
+
+  constructor(rla: RouterLinkActive) {
+    rla.routerLinkActive = 'active';
+  }
+
+
 
 }
