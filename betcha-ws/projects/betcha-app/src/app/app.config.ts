589c4e5c8230469e4e34268b561b6d2d6078f751
Kobi Hari <kobi2294@yahoo.com>
2024-06-10 03:48:25 +0300

    ya-betcha app config

--- betcha-ws/projects/betcha-app/src/app/app.config.ts
+++ betcha-ws/projects/betcha-app/src/app/app.config.ts
@@ -1,14 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, isDevMode } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideFirebaseServices, provideVersion } from '@lib';
+import { provideFirebaseServices, provideLib, provideLoginUi, providePwa, provideVersion } from '@lib';
 import { environment } from '../environments/environment';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes, withComponentInputBinding()), 
     provideVersion(),
+    provideLib(),
+    providePwa(),
     provideFirebaseServices(environment.firebaseConfig, environment.simulation),   
+    provideLoginUi(), 
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
