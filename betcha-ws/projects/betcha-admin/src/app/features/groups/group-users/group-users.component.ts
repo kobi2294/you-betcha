936b537ab84b4c517fb1f4e0099395ecb4553bcc
Kobi Hari <kobi2294@yahoo.com>
2024-06-13 19:52:20 +0300

    users manager now adds and removes group admins

--- betcha-ws/projects/betcha-admin/src/app/features/groups/group-users/group-users.component.ts
+++ betcha-ws/projects/betcha-admin/src/app/features/groups/group-users/group-users.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, inject } from '@angular/core';
-import { ApiService, PagesModule, SharedModule, WorkflowService } from '@lib';
+import { ApiService, AuthStore, PagesModule, SharedModule, WorkflowService } from '@lib';
 import { GroupDetailsStore } from '../group-details/store/group-details.store';
 import { toRecord } from '@tscommon';
 import { buildUsersVm } from './group-users.vm';
@@ -16,6 +16,7 @@ export default class GroupUsersComponent {
   readonly store = inject(GroupDetailsStore);
   readonly workflow = inject(WorkflowService);
   readonly api = inject(ApiService);
+  readonly auth = inject(AuthStore);
 
   readonly vm = computed(() => buildUsersVm(this.store.members(), this.store.group()?.admins || []));
 
@@ -29,11 +30,20 @@ export default class GroupUsersComponent {
     }));
 
     if (result && result.ok) {
+      
     }
 
   }
 
   toggleAdmin(id: string) {
+    const group = this.store.group();
+    if (!group) return;
+    const isAdmin = group.admins.includes(id);
+    if (isAdmin) {
+      this.store.removeAdmin(id);
+    } else {
+      this.store.addAdmin(id);
+    }
       
   }
 
