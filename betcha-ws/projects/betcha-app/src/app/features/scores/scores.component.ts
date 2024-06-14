5b9bd620d906d019bcb9e5dea1309cb8b0924b37
Kobi Hari <kobi2294@yahoo.com>
2024-06-14 18:55:53 +0300

    scores fixes

--- betcha-ws/projects/betcha-app/src/app/features/scores/scores.component.ts
+++ betcha-ws/projects/betcha-app/src/app/features/scores/scores.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed, inject } from '@angular/core';
 import { GameStore } from '../../stores/game/game.store';
-import { PagesModule, SharedModule } from '@lib';
+import { GameVm, PagesModule, SharedModule } from '@lib';
 
 @Component({
   selector: 'app-scores',
@@ -13,5 +13,19 @@ export default class ScoresComponent {
   game = inject(GameStore);
 
   table = computed(() => this.game.vm().table);
+  pointsLeft = computed(() => pointsLeftInGame(this.game.vm()));
 
+}
+
+function pointsLeftInGame(game: GameVm) {
+  const inProgress = game.inProgressMatches
+    .map(m => m.points)
+    .reduce((a, b) => a + b, 0);
+
+  const inFuture = game.futureMatches
+    .map(m => m.points)
+    .reduce((a, b) => a + b, 0);
+
+  return inProgress + inFuture;
+  
 }
