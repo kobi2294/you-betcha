518fa22c5aa51beacd4a5585a23096adfd80dadc
Kobi Hari <kobi2294@yahoo.com>
2024-06-15 15:06:02 +0300

    slides engine

---
+++ betcha-ws/projects/betcha-dashboard/src/app/models/slide.model.ts
@@ -0,0 +1,156 @@
+import { DbModel } from '@tscommon';
+
+type SlideGen<T> = () => T;
+type MatchSlideGen<T> = (matchId: string) => T;
+
+export const finalCountdounSlide: SlideGen<FinalCountdownSlide> = () => ({
+  type: 'final-countdown',
+  topLeft: false,
+  topRight: false,
+  message: 'Last chance to guess!!!',
+});
+export interface FinalCountdownSlide {
+  readonly type: 'final-countdown';
+  readonly topLeft: false;
+  readonly topRight: false;
+  readonly message: string;
+}
+
+export const top3Slide: SlideGen<Top3Slide> = () => ({
+  type: 'top-3',
+  topLeft: true,
+  topRight: true,
+  message: true,
+});
+export interface Top3Slide {
+  readonly type: 'top-3';
+  readonly topLeft: true;
+  readonly topRight: true;
+  readonly message: true;
+}
+
+export const comingUpSlide: MatchSlideGen<ComingUpSlide> = (
+  matchId: string
+) => ({
+  type: 'coming-up',
+  topLeft: true,
+  topRight: true,
+  message: 'Dont forget to guess!',
+  matchId,
+});
+export interface ComingUpSlide {
+  readonly type: 'coming-up';
+  readonly topLeft: true;
+  readonly topRight: true;
+  readonly message: string;
+  readonly matchId: string;
+}
+
+export const nowPlayingSlide: MatchSlideGen<NowPlayingSlide> = (
+  matchId: string
+) => ({
+  type: 'now-playing',
+  topLeft: true,
+  topRight: true,
+  message: true,
+  matchId,
+});
+export interface NowPlayingSlide {
+  readonly type: 'now-playing';
+  readonly topLeft: true;
+  readonly topRight: true;
+  readonly message: true;
+  readonly matchId: string;
+}
+
+export const matchSummarySlide: MatchSlideGen<MatchSummarySlide> = (
+  matchId: string
+) => ({
+  type: 'match-summary',
+  topLeft: true,
+  topRight: true,
+  message: true,
+  matchId,
+});
+export interface MatchSummarySlide {
+  readonly type: 'match-summary';
+  readonly topLeft: true;
+  readonly topRight: true;
+  readonly message: true;
+  readonly matchId: string;
+}
+
+export const recentHighestScorersSlide: SlideGen<
+  RecentHighestScorersSlide
+> = () => ({
+  type: 'recent-highest-scorers',
+  topLeft: true,
+  topRight: true,
+  message: true,
+});
+export interface RecentHighestScorersSlide {
+  readonly type: 'recent-highest-scorers';
+  readonly topLeft: true;
+  readonly topRight: true;
+  readonly message: true;
+}
+
+export type Surprise = {
+  matchId: string;
+  playerId: string;
+  guess: DbModel.GuessValue;
+  correctCount: number;
+  totalCount: number;
+};
+
+export const surpriseHuntersSlide: (s: Surprise) => SurpriseHuntersSlide = (
+  surprise
+) => ({
+  type: 'surprise-hunters',
+  topLeft: true,
+  topRight: true,
+  message: true,
+  surprise,
+});
+export interface SurpriseHuntersSlide {
+  readonly type: 'surprise-hunters';
+  readonly topLeft: true;
+  readonly topRight: true;
+  readonly message: true;
+  readonly surprise: Surprise;
+}
+
+export const soloSummarySlide: SlideGen<SoloSummarySlide> = () => ({
+  type: 'solo-summary',
+  topLeft: true,
+  topRight: true,
+  message: true,
+});
+export interface SoloSummarySlide {
+  readonly type: 'solo-summary';
+  readonly topLeft: true;
+  readonly topRight: true;
+  readonly message: true;
+}
+
+export type Slide =
+  | FinalCountdownSlide
+  | Top3Slide
+  | ComingUpSlide
+  | NowPlayingSlide
+  | MatchSummarySlide
+  | RecentHighestScorersSlide
+  | SurpriseHuntersSlide
+  | SoloSummarySlide;
+
+export type SlideType = Slide['type'];
+export const slideGens = {
+  finalCountdounSlide,
+  top3Slide,
+  comingUpSlide,
+  nowPlayingSlide,
+  matchSummarySlide,
+  recentHighestScorersSlide,
+  surpriseHuntersSlide,
+  soloSummarySlide,
+} as const;
---
+++ betcha-ws/projects/betcha-dashboard/src/app/models/slides-organizer.ts
@@ -0,0 +1,108 @@
+import { GameVm } from '@lib';
+import { Slide, Surprise, slideGens } from './slide.model';
+
+export function slidesForState(gameVm: GameVm): Slide[] {
+  // during countdown time - 20 seconds before match start, until 10 seconds after - show only one slide - "final countdown"
+  if (shouldShowCountDown(gameVm)) return [slideGens.finalCountdounSlide()];
+
+  // otherwise:
+  // any other time
+  // - "top 3" slide
+  const res: Slide[] = [];
+  res.push(slideGens.top3Slide());
+
+  // if there were matches in the past 24 hours
+  // - for each match show "match summary" slide
+  // - construct table of 24 hours ago and of now, and show the "recent highest scorers" slide
+  const matchesInPast24Hours = gameVm.pastMatches.filter(
+    (m) => m.dateValue > Date.now() - 24 * 60 * 60 * 1000
+  );
+
+  if (matchesInPast24Hours.length > 0) {
+    matchesInPast24Hours.forEach((m) =>
+      res.push(slideGens.matchSummarySlide(m.id))
+    );
+    res.push(slideGens.recentHighestScorersSlide());
+  }
+
+  // if the group has solos in the history - show the last 3 solos in the "solo - summary" slide
+  const anySolos = gameVm.table.some((g) => g.soloCount > 0);
+  if (anySolos) res.push(slideGens.soloSummarySlide());
+
+  // if the group has any surprise catcher - show the last 3 surprise catchers in the "surprise hunters" slide
+  // suprise hunter is a case where somebody guessed correctly a score, that had less than 10% statistical chance of happening
+  const surprises = getSurprises(gameVm);
+  surprises.forEach((s) => res.push(slideGens.surpriseHuntersSlide(s)));
+
+
+  // during matches
+  // - for each match in progress - show now playing slide
+  const inProgressMatches = gameVm.inProgressMatches;
+  if (inProgressMatches.length > 0) {
+    inProgressMatches.forEach((m) => res.push(slideGens.nowPlayingSlide(m.id)));
+  } else {
+    // between matches:
+    // - for each match coming up - show coming up slide
+    const nextMatches = gameVm.nextMacthes;
+    if (nextMatches.length > 0) {
+      nextMatches.forEach((m) => res.push(slideGens.comingUpSlide(m.id)));
+    }
+  }
+
+  return res;
+}
+
+// what data do we need:
+// 1. Next matches
+// 2. Matches ended in the past 24 hours
+// 3. All historical matches
+// 4. Table of now
+// 5. Table of 24 hours ago
+
+function getSurprises(gameVm: GameVm): Surprise[] {
+    const res: Surprise[] = [];
+    const pastMatches = gameVm.pastMatches;
+
+    for (const match of pastMatches) {
+        const totalCount = match.globalStatistics.away + match.globalStatistics.home + match.globalStatistics.tie;
+        if (totalCount < 20) continue;
+
+        const correctCount = match.globalStatistics[match.correctGuess];
+        const correctChance = correctCount / totalCount;
+        if (correctChance >= 0.1) continue; // its not considered surprise if the chance is 10% or more
+
+        // now add all the correct guessers
+        for (const userScore of match.userScores) {
+            if (userScore.guess === match.correctGuess) {
+                res.push({matchId: match.id, playerId: userScore.id, guess: userScore.guess, correctCount, totalCount});
+            }
+        }
+    }
+
+    return res;
+}
+
+function shouldShowCountDown(gameVm: GameVm) {
+  // we should show the countdown in one of two cases:
+  // if the matches in progress are less than 10 seconds old
+  // if the matches next matches are less than 20 seconds away
+  const now = Date.now();
+  const nextMatches = gameVm.nextMacthes;
+  const inProgressMatches = gameVm.inProgressMatches;
+
+  if (inProgressMatches.length > 0) {
+    const inProgressMatch = inProgressMatches[0];
+    if (now - inProgressMatch.dateValue < 10000) {
+      return true;
+    }
+  }
+
+  if (nextMatches.length > 0) {
+    const nextMatch = nextMatches[0];
+    if (nextMatch.dateValue - now < 20000) {
+      return true;
+    }
+  }
+
+  return false;
+}
